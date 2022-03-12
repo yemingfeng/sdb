0f626b8bd324a9acd6aa4cf01374298f2cbb405a
yemingfeng <393162333@qq.com>
2022-03-13 01:21:36 +0800

    Add: sdb.yaml

--- scripts/build_protobuf.sh
+++ scripts/build_protobuf.sh
@@ -1,4 +1,4 @@
 rm -rf pkg/protobuf/*.go
 protoc --go_out=./ --go-grpc_out=./ ./pkg/sdb-protobuf/*.proto --go-grpc_opt=require_unimplemented_servers=false
-protoc --grpc-gateway_out . --grpc-gateway_opt logtostderr=true --grpc-gateway_opt generate_unbound_methods=true --grpc-gateway_opt grpc_api_configuration=./pkg/sdb-protobuf/sdb.yaml ./pkg/sdb-protobuf/*.proto
-protoc --openapiv2_out . --openapiv2_opt grpc_api_configuration=./pkg/sdb-protobuf/sdb.yaml ./pkg/sdb-protobuf/sdb.proto
+protoc --grpc-gateway_out . --grpc-gateway_opt logtostderr=true --grpc-gateway_opt generate_unbound_methods=true --grpc-gateway_opt grpc_api_configuration=./api/sdb.yaml ./pkg/sdb-protobuf/*.proto
+protoc --openapiv2_out . --openapiv2_opt grpc_api_configuration=./api/sdb.yaml ./pkg/sdb-protobuf/sdb.proto
