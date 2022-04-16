89f68eec182ccf10cf41f745f129015cdc45df46
yemingfeng <393162333@qq.com>
2022-04-16 20:35:29 +0800

    Mod: refactor code

--- internal/store/store.go
+++ internal/store/store.go
@@ -5,6 +5,27 @@ import (
 	"github.com/yemingfeng/sdb/internal/util"
 )
 
+type Store interface {
+	NewBatch() Batch
+	Close() error
+}
+
+type Batch interface {
+	Get(key []byte) ([]byte, error)
+	Set(key []byte, value []byte) error
+	Del(key []byte) error
+	Iterate(opt *PrefixIteratorOption, handle func([]byte, []byte) error) error
+	Commit() error
+	Close()
+}
+
+type PrefixIteratorOption struct {
+	Prefix []byte
+
+	Offset int32
+	Limit  uint32
+}
+
 const (
 	PEBBLE string = "pebble"
 	BADGER string = "badger"
